f585d418778436eb0fd7d8389d7c5b77d24de3af
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2025-07-13 00:38:42 -0400

    working rtmidi to chord analysis

--- midi_receive.py
+++ midi_receive.py
@@ -1,63 +1,50 @@
-#!/usr/bin/env python3
-"""A demonstration MIDI receiver which displays events."""
-################################################################
-# Written in 2018-2021 by Garth Zeglin <garthz@cmu.edu>
+import time, threading
 
-# To the extent possible under law, the author has dedicated all copyright
-# and related and neighboring rights to this software to the public domain
-# worldwide. This software is distributed without any warranty.
-
-# You should have received a copy of the CC0 Public Domain Dedication along with this software.
-# If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-
-################################################################
-# Standard Python libraries.
-import argparse, time, platform
-
-# For documentation on python-rtmidi: https://pypi.org/project/python-rtmidi/
 import rtmidi
 
+from music21 import chord
+
 ################################################################
-def midi_received(data, unused):
-    msg, delta_time = data
-    if len(msg) > 2:
-        if msg[0] == 153: # note on, channel 9
-            key = (msg[1] - 36) % 16
-            row = key // 4
-            col = key % 4
-            velocity = msg[2]
-            print("MPD218 Pad (%d, %d): %d" % (row, col, velocity))
+class ChordAnalyzer:
+    def __init__(self):
+        self.current_notes = set()
+        self.lock = threading.Lock()
+        self.midi_in = rtmidi.MidiIn()
+    
+    def midi_callback(self, data, _):
+        msg, _ = data
+        # print("MIDI message: ", msg)
+
+        if len(msg) == 3:
+            status, note, velocity = msg
+            with self.lock:
+                if status == 144 and velocity > 0:
+                    self.current_notes.add(note)
+                elif status == 128 or (status == 144 and velocity == 0):
+                    self.current_notes.discard(note)
+    
+    def get_current_chord(self):
+        with self.lock:
+            c = chord.Chord(self.current_notes).pitchedCommonName
+            if c != "empty chord":
+                return c
+    
+    def start_monitoring(self):
+        self.midi_in.set_callback(self.midi_callback)
+        available_ports = self.midi_in.get_ports()
+        if available_ports:
+            self.midi_in.open_port(0)
+            print(available_ports[0])
+        else:
+            print("No available MIDI ports :(")
             return
-    print("MIDI message: ", msg)
-    
-################################################################
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--midi", type=str, default = "Roland Digital Piano", help = "Keyword identifying the MIDI input device (default: %(default)s).")
-    args = parser.parse_args()
-
-    # Initialize the MIDI input system and read the currently available ports.
-    midi_in = rtmidi.MidiIn()
-    for idx, name in enumerate(midi_in.get_ports()):
-        if args.midi in name:
-            print("Found preferred MIDI input device %d: %s" % (idx, name))
-            midi_in.open_port(idx)
-            midi_in.set_callback(midi_received)
-            break
-        else:
-            print("Ignoring unselected MIDI device: ", name)
-
-    if not midi_in.is_port_open():
-        if platform.system() == 'Windows':
-            print("Virtual MIDI inputs are not currently supported on Windows, see python-rtmidi documentation.")
-        else:
-            print("Creating virtual MIDI input.")
-            midi_in.open_virtual_port(args.midi)
-    
-    if not midi_in.is_port_open():
-        print("No MIDI device opened, exiting.")
-
-    else:
-        print("Waiting for input.")
+        
         while True:
-            time.sleep(1)
+            chord = self.get_current_chord()
+            if chord:
+                print(f"Current chord: {chord}")
+            time.sleep(0.1)
+
+if __name__ == "__main__":
+    analyzer = ChordAnalyzer()
+    analyzer.start_monitoring()
